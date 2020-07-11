6acf3ab4ead82246b4bfd383eb7cb56d9b59b382
angeloesaki <angeloesaki@gmail.com>
2020-07-11 11:22:16 +0900

    数値をランダムに配置

--- js/main.js
+++ js/main.js
@@ -11,6 +11,11 @@
     getEl() {
       return this.el;
     }
+
+    activate(num) {
+      this.el.classList.remove("pressed");
+      this.el.textContent = num;
+    }
   }
 
   //パネルを管理
@@ -32,7 +37,23 @@
         board.appendChild(panel.getEl());
       });
     }
+
+    activate() {
+      const nums = [0, 1, 2, 3];
+
+      this.panels.forEach((panel) => {
+        //ランダムな位置から要素を一つ取り出す
+        //[0]を付け足すことで返り値が配列でなくなる
+        const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+        panel.activate(num);
+      });
+    }
   }
 
   const board = new Board();
+
+  const button = document.getElementById("button");
+  button.addEventListener("click", () => {
+    board.activate();
+  });
 }
